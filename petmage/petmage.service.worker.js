5da81db329a023f205a070e3ea300c92732b4275
Kilolo <kkaykayboy@icloud.com>
2026-04-13 06:04:45 -0400

    update pmnh

--- petmage/petmage.service.worker.js
+++ petmage/petmage.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776072144|210800997';
+const CACHE_VERSION = '1776074653|2719528591';
 /** @type {string} */
 const CACHE_PREFIX = 'bluigi-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
