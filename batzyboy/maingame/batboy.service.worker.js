01f8830703b5f2af7868265d12cb7d6af77cd49d
Kilolo <kkaykayboy@icloud.com>
2026-05-01 19:18:43 -0400

    final update v1 2 3
    lets see

--- batzyboy/maingame/batboy.service.worker.js
+++ batzyboy/maingame/batboy.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777676504|875834662';
+const CACHE_VERSION = '1777677501|1872960613';
 /** @type {string} */
 const CACHE_PREFIX = 'batzyboy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
