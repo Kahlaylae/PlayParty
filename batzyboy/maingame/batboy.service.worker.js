7761e85bbeb071322d47dd194547907a5c5fa4c6
Kilolo <kkaykayboy@icloud.com>
2026-05-01 20:22:48 -0400

    okay new sprite sheet for heart ui
    heart ui

--- batzyboy/maingame/batboy.service.worker.js
+++ batzyboy/maingame/batboy.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777677501|1872960613';
+const CACHE_VERSION = '1777681346|5717466957';
 /** @type {string} */
 const CACHE_PREFIX = 'batzyboy-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
